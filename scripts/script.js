03b83b881cc8fbc486d5a62fd9b8886386e7a0da
Antheny Wells <anthenywells@Go-away.local>
2019-02-20 15:07:46 -0500

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= $(window).height()) {
+  $('#header')
+    .css('background','#2948ff')
+  $('.resume')
+    .css('background','#262626');
+  } else {
+  $('#header')
+    .css('background','transparent');
+  $('.resume')
+    .css('background','#2948ff');
+  }
+});
+
+
+// $("a[href^='#']").click(function(e) {
+// 	var position = $($(this).attr("href")).offset().top;
+// 	$("body, html").animate({
+// 		scrollTop: position
+// 	}, 400, 'linear' );
+// });
